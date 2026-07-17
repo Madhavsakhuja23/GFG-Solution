a9acf67c3f1d783e4b7170bc06945953576f74f0
Madhav Sakhuja <msakhuja22@gmail.com>
2026-07-17 17:29:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.java
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.java
@@ -1,12 +1,3 @@
-class Pair{
-    int first;
-    int second;
-
-    Pair(int f, int s){
-        first=f;
-        second=s;
-        }
-}
 class Solution {
     public boolean isCycle(int V, int[][] edges) {
         // Code here
@@ -15,39 +6,50 @@ class Solution {
             adj.add(new ArrayList<>());
         }
         for(int i=0;i<edges.length;i++){
-            int u =edges[i][0];
-            int v =edges[i][1];
+            int u = edges[i][0];
+            int v = edges[i][1];
             
             adj.get(u).add(v);
             adj.get(v).add(u);
         }
         int visited[] = new int[V];
+        
         for(int i=0;i<V;i++){
             if(visited[i]==0){
-                if(detectCycle(i, adj,visited)){
+                if(detectCycle(i, adj, visited)){
                     return true;
                 }
             }
         }
         return false;
     }
-    public boolean detectCycle(int node, ArrayList<ArrayList<Integer>> adj, int visited[]){
-        visited[node]=1;
+    public boolean detectCycle(int node,ArrayList<ArrayList<Integer>> adj , int vis[] ){
+        vis[node]=1;
         Queue<Pair> q = new LinkedList<>();
         q.add(new Pair(node, -1));
         while(!q.isEmpty()){
-            int f=q.peek().first;
-            int parent=q.peek().second;
-            q.remove();
-            for(int it: adj.get(f)){
-                if(visited[it]==0){
-                    visited[it]=1;
-                    q.add(new Pair(it,f));
-                }else if(it!=parent){
+            Pair curr = q.poll();
+            int no = curr.n;
+            int pn = curr.Parentn;
+            
+            for(int it: adj.get(no)){
+                if(vis[it]==0){
+                    vis[it]=1;
+                    q.add(new Pair(it, no));
+                }
+                else if(it!=pn){
                     return true;
                 }
             }
         }
         return false;
+    }
+}
+class Pair{
+    int n;
+    int Parentn;
+    Pair(int no, int pn){
+        n=no;
+        Parentn=pn;
     }
 }
